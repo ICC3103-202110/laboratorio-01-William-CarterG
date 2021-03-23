648db7bc357b0fa06824d069631e336ae4e2c166
William-CarterG <wgcarter@miuandes.cl>
2021-03-22 21:17:03 -0300

    3rd commit ; not using OOP anymore...

--- Lab 1 OOP.py
+++ Lab 1 OOP.py
@@ -1,43 +1,60 @@
 import random
-class Memorice() :
-    """
-    Representación del juego Memorice
 
-    """
-cartas = int(input("Ingrese el número de cartas a jugar :\n"))
-    def _init_(self, cartas) :
-        #Se crea un tablero vacío. Por temas de orden cada fila va a tener un máximo arbitrario de 6 cartas
-        self.width  = 6  
+"""
+Crea un tablero del juego Memorice
 
-        #Para saber cuántas filas crear hay ver si el número n es divisible por 6
-        if cartas/6 == int(cartas/6) :
-            self.height = int(2*cartas/6)
-        else :
-            self.height = int(2*cartas/6) + 1
+"""
+def Board() :
+    cards = int(input("Ingrese el número de cartas a jugar :\n"))
 
-        #Ahora crearé el tablero y le agregaré las cartas de forma aleatoria 
-        c = []
-        for i in range(cartas) :
-            c.append(i+1)
-            c.append(i+1)  
+    #Se crea un tablero vacío. Por temas de orden cada fila va a tener un máximo arbitrario de 6 cartas
+    width  = 6  
 
-        self.board = []
-        for i in range(self.height) :
-            row = []
-            for j in range(self.width) :
-                if len(c) > 0 :
-                    x = random.choice(c)
-                    c.remove(x)
-                    row.append(x)
-                else :
-                    row.append("")   #Esta condición es necesaria, pues las cartas no tienen porque utilizar todo el tablero, pues este fue creado con tamaños arbitrarios
-            self.board.append(row)
+    #Para saber cuántas filas crear hay ver si el número n es divisible por 6
+    if cards/6 == int(cards/6) :
+        height = int(2*cards/6)
+    else :
+        height = int(2*cards/6) + 1
 
-    def print(self) :
-        
+    #Ahora crearé el tablero y le agregaré las cartas de forma aleatoria 
+    c = []
+    for i in range(cards) :
+        c.append(i+1)
+        c.append(i+1)  
 
+    board = []
+    for i in range(height) :
+        row = []
+        for j in range(width) :
+            if len(c) > 0 :
+                x = random.choice(c)
+                c.remove(x)
+                row.append(x)
+            else :
+                row.append("")   #Esta condición es necesaria, pues las cartas no tienen porque utilizar todo el tablero.
+        board.append(row)
+    return board
 
 
-        
+"""
+Imprime las condiciones del tablero actual 
+"""
+def printb(board) :
+    height = len(board)
+    width = 6    #Definí que siempre será igual. De todas maneras se podría hacer para un caso general (pero en este lab nunca habrá otro caso)
+    for i in range(height):
+        print("-" * width * 9 + "-") # "-" por cada cuadro (cada uno tiene 9 caractéres)
+        for j in range(width):
+            if board[i][j] != "":
+                print("|*(%d, %d)*"%(i, j), end="")
+            else:
+                print("|        ", end="")
+        print("|")
+    print("--" * width + "-")
+
+board = Board()
+printb(board)
+
+    
 
 
