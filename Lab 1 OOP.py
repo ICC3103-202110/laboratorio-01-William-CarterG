f6ffa0e15486b056bda6291da6522a940505817f
William-CarterG <wgcarter@miuandes.cl>
2021-03-24 13:32:39 -0300

    6th commit; almost ready

--- Lab 1 OOP.py
+++ Lab 1 OOP.py
@@ -1,10 +1,10 @@
 import random
-
+from time import sleep
 """
 Crea un tablero del juego Memorice
 
 """
-def Board(cards) :
+def Boards(cards) :
     #Se crea un tablero vacío. Por temas de orden cada fila va a tener un máximo arbitrario de 6 cartas
     width  = 6  
 
@@ -37,16 +37,17 @@ def Board(cards) :
 """
 Imprime las condiciones del tablero actual 
 """
-def printb(board, found) :
+def printb(board, found) :      
+    height = len(board)
+    width = 6    #Definí que siempre será igual. De todas maneras se podría hacer para un caso general (pero en este lab nunca habrá otro caso)
+
     #Esta fue la forma más simple que se me ocurrio de como imprimir cualquier carta :(
     for i in range(height) :
         for j in range(width) :
-        for pair in found :
-            if (i,j)==(pair[0],pair[1]):
-                board[i][j] = str(board[i][j])          
+            for pair in found :
+                if (i,j)==(pair[0],pair[1]):
+                    board[i][j] = str(board[i][j])    
 
-    height = len(board)
-    width = 6    #Definí que siempre será igual. De todas maneras se podría hacer para un caso general (pero en este lab nunca habrá otro caso)
     for i in range(height):
         print("-" * width * 9 + "-") # "-" por cada cuadro (width) por el espacio que ocupan (cada uno tiene 9 caractéres)
         for j in range(width):
@@ -58,9 +59,10 @@ def printb(board, found) :
             else : #Cómo no son str, sólo mostraré la coordenada en que se encuentran
                 print("|*(%d, %d)*"%(i, j), end="")
         print("|")
-    print("--" * width + "-")
+    print("-" * width * 9 + "-")
 
-def Memorice(board, found) :
+def Memorice(board) :
+    found = [] # Es el nombre que le di a lista de cartas seleccionadas
     val = 0 #validity of coordinates
     while val == 0 :
         coor1 = input("Ingresa las coordenadas de la primera carta (i,j) :\n")
@@ -68,13 +70,11 @@ def Memorice(board, found) :
 
         #Sólo por las dudas, voy a validar que las coordenadas estén bien (la ayudante nos dijo que era siempre
         # recomendable). Perdón por la línea gigante:( Pero son muchas condiciones que hay que comprobar...
-        if (0 <= (int(coor1_x) and int(coor1_y)))
-        and int(coor1_x) < len(board) and int(coor1_y) < 6 
-        and (coor1[1].isnumeric() and coor1[-2].isnumeric()) == True :
-
+        if (0 <= (int(coor1_x) and int(coor1_y))) and int(coor1_x) < len(board) and int(coor1_y) < 6 and (coor1[1].isnumeric() and coor1[-2].isnumeric()) == True :
             c_1  = int(coor1_x), int(coor1_y)
             found.append(c_1)
             printb(board, found)
+            sleep(1)
             val = 1
 
     val = 0
@@ -83,36 +83,95 @@ def Memorice(board, found) :
         coor2_x , coor2_y = coor2[1], coor2[-2]
 
         #Perdón... denuevo  :(
-        if (0 <= (int(coor2_x) and int(coor2_y))) 
-        and int(coor2_x) < len(board) and int(coor2_y) < 6 
-        and (coor2[1].isnumeric() and coor2[-2].isnumeric()) == True :
-            c_2 = int(coor2_x), int(coor2_y)
+        if (0 <= (int(coor2_x) and int(coor2_y))) and int(coor2_x) < len(board) and int(coor2_y) < 6 and (coor2[1].isnumeric() and coor2[-2].isnumeric()) == True :
+            c_2 = int(coor2_x), int(coor2_y) and (board)
             found.append(c_2)
             printb(board, found)
+            sleep(1)
             val = 1
 
     return c_1, c_2
     
 def Interfaz():
     print("¡Bienvenido a Memorice!\n")
-    juego = 1 # int(input("Ingresa 1 en la terminal para jugar!!. Si deseas dalir, ingresa 0"))
-    while juego != 0 :
-        print("""***INSTRUCCIONES***\nEn este juego habrán una cierta cantidad de pares de cartas iguales, pero estas estarán
+    sleep(1)
+    print("***INSTRUCCIONES***\n")
+    sleep(2)
+    print("""En este juego habrán una cierta cantidad de pares de cartas iguales, pero estas estarán
 desordenadas y ocultas en el tablero.\nDos jugadores se turnarán para tratar de encontrar los pares de cartas iguales,
 eligiendo 2 cartas en cada turno. Si las cartas elegidas son iguales, el jugador que las
-eligió sumará un punto y repetirá su turno. Si no, es turno del otro\nEl juego termina cuando encuentren todos
-los pares\n ¡¡¡Buena suerte!!!""")
+seleccionó sumará un punto y repetirá su turno. Si no, es turno del otro\nEl juego termina cuando encuentren todos los pares\n""")
+    sleep(5)
+    print("¡¡¡Buena suerte!!!")
+    sleep(0.5)
+    
+    val = 0
+    while val !=1:
+        cards = (input("Ingrese el número de cartas a jugar :\n"))
+        if cards.isnumeric() ==  True :
+            cards = int(cards)
+            val = 1
+    board = Boards(cards)
+    p_1, p_2 = 0, 0
+    
+    turn = 0
+    found  = 0
+    f = []
+    
+    while (turn/2) == int(turn/2) :
+        print(found, cards)
+        #Antes del turno revisaré si el juego se ha acabado     
+        print("Turno del jugador 1")
+        printb(board, f)
 
-        cards = int(input("Ingrese el número de cartas a jugar :\n"))
-        board = Boards(cards)
-        p_1, p_2 = 0, 0
-        
-        turn = 0
-        found  = []
-        while len(found) != 2*cards
-            while (turn/2) == int(turn/2) :
-               c_1, c_2 = Memorice(board)
+        c_1, c_2 = Memorice(board)
+        if board[c_1[0]][c_1[1]] == board[c_2[0]][c_2[1]] :
+            board[c_1[0]][c_1[1]] = ""
+            board[c_2[0]][c_2[1]] = ""
+            found +=1
+            p_1 += 1
+            if p_1 == 1 :
+                print("El jugador 1 tiene %d punto"%p_1)
+            else :
+                print("El jugador 1 tiene %d puntos"%p_1)
+            if found == cards :
+                if p_1 == p_2 :
+                    return print("Ha habido un empate")
+                elif p_1 > int(cards/2) :
+                    return print("El ganador es el jugador 1 con %d puntos"%p_1)
+                elif p_2 > int(cards/2) :
+                    return print("El ganador es el jugador 2 con %d puntos"%p_2)
+            turn += 2
+        else :
+            board[c_1[0]][c_1[1]] = int(board[c_1[0]][c_1[1]])
+            board[c_2[0]][c_2[1]] = int(board[c_2[0]][c_2[1]])
+            turn += 1
 
+    while (turn/2) != int(turn/2) :
+        if found == cards :
+            if p_1 > int(cards/2) :
+                return print("El ganador es el jugador 1 con %d puntos"%p_1)
+            elif p_2 > int(cards/2) :
+                return print("El ganador es el jugador 2 con %d puntos"%p_2)
 
+        print("Es turno del jugador 2")
+        printb(board,f)
+        c_1, c_2 = Memorice(board)
+        print(c_1,c_2)
+        if board[c_1[0]][c_1[1]] == board[c_2[0]][c_2[1]] :
+            board[c_1[0]][c_1[1]] = ""
+            board[c_2[0]][c_2[1]] = ""
+            found +=1
+            p_2 += 1
+            if p_2 == 1 :
+                print("El jugador 2 tiene %d punto"%p_2)
+            else :
+                print("El jugador 2 tiene %d puntos"%p_2)
+            turn += 2
+        else :
+            board[c_1[0]][c_1[1]] = int(board[c_1[0]][c_1[1]])
+            board[c_2[0]][c_2[1]] = int(board[c_2[0]][c_2[1]])
+            turn += 1
+            print(turn)
 
-
+(Interfaz())
